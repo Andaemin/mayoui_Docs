bb4807dd35cc6e6bd44c2004e533e443eec24898
andaemin <eoals0216@gmail.com>
2026-06-03 19:11:09 +0900

    add : TossFaceFont 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,53 +1,32 @@
-import Image from "next/image";
 import { MayoBtn } from "@mayoui/ui";
 
 export default function Home() {
     return (
         <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-            <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-                <Image className="dark:invert" src="/next.svg" alt="Next.js logo" width={100} height={20} priority />
-
+            <main className="flex w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
                 <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+                    <h1 className="max-w-xs font-bold text-4xl">🧪 MayoUI</h1>
                     <MayoBtn variant="primary">Click Me</MayoBtn>
-                    <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-                        To get started, edit the page.tsx file.
+                    <h1 className="max-w-sm text-3xl font-semibold leading-11 tracking-normal text-black dark:text-zinc-50">
+                        한국인이 쓰기 편한 UI를 위해
                     </h1>
                     <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-                        Looking for a starting point or more instructions? Head over to{" "}
+                        저희는 가독성과 심미성을 높이기 위해{" "}
                         <a
                             href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                            className="font-medium text-zinc-950 dark:text-zinc-50"
+                            className="font-semibold text-zinc-950 dark:text-zinc-50"
                         >
-                            Templates
+                            TossFaceFont
                         </a>{" "}
-                        or the{" "}
+                        와{" "}
                         <a
                             href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                            className="font-medium text-zinc-950 dark:text-zinc-50"
+                            className="font-semibold text-zinc-950 dark:text-zinc-50"
                         >
-                            Learning
+                            Pretendard
                         </a>{" "}
-                        center.
+                        폰트를 사용 했습니다.
                     </p>
-                </div>
-                <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-                    <a
-                        className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-                        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <Image className="dark:invert" src="/vercel.svg" alt="Vercel logomark" width={16} height={16} />
-                        Deploy Now
-                    </a>
-                    <a
-                        className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-                        href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Documentation
-                    </a>
                 </div>
             </main>
         </div>
